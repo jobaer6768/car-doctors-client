ed559006fff8ee3fe71f93b300d6844b93e8330d
Md Jobaer Hossain <120457646+jobaer6768@users.noreply.github.com>
2023-10-28 11:30:43 +0600

    updated navbar, footer and banner section

--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -1,10 +1,14 @@
 import { Outlet } from "react-router-dom";
+import Footer from "../shared/Footer/Footer";
+import Navbar from "../shared/Navbar/Navbar";
 
 
 const Main = () => {
     return (
-        <div>
+        <div className="max-w-6xl mx-auto">
+            <Navbar></Navbar>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -7,5 +7,5 @@ import Router from './routes/Router'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <RouterProvider router={Router}></RouterProvider>
-  </React.StrictMode>,
+  </React.StrictMode>
 )
